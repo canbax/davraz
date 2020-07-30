29af3e6774f7017570d1186c80be75a782f011f0
canbax <yusufsaidcanbaz@gmail.com>
2020-07-30 08:33:30 +0300

    start with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import cytoscape from 'cytoscape';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    window['cy'] = cytoscape({
+      elements: {
+        nodes: [
+          {
+            data: { id: 'a' }
+          },
+
+          {
+            data: { id: 'b' }
+          }
+        ],
+        edges: [
+          {
+            data: { id: 'ab', source: 'a', target: 'b' }
+          }
+        ]
+      },
+
+      // so we can see the ids
+      style: [
+        {
+          selector: 'node',
+          style: {
+            'label': 'data(id)'
+          }
+        }
+      ],
+      container: document.getElementById('cy')
+    });
+  }
   title = 'graph-imager';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
