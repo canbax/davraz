6ca851b37a6901c816cbc62e4530e773711ece67
canbax <yusufsaidcanbaz@gmail.com>
2020-09-05 16:53:38 +0300

    edge hover animation

--- src/app/config/general-cy-style.ts
+++ src/app/config/general-cy-style.ts
@@ -1,7 +1,7 @@
 export const GENERAL_CY_STYLE: any =
   [
     {
-      selector: ".hover",
+      selector: "node.hover",
       style: {
         "background-fill": "radial-gradient",
         "background-gradient-stop-colors": "#e0f7fa #18ffff",
@@ -9,6 +9,19 @@ export const GENERAL_CY_STYLE: any =
         "border-style": "dotted",
         "border-width": 3,
         "border-color": "#18ffff",
+      }
+    },
+    {
+      selector: "edge.hover",
+      style: {
+        "line-fill": "linear-gradient",
+        "line-gradient-stop-colors": "#e0f7fa #006064",
+        "line-gradient-stop-positions": "0 50 100",
+        "line-style": "dashed",
+        "line-dash-pattern": [1,2],
+        "line-dash-offset": 48,
+        "transition-property": "line-dash-offset",
+        "transition-duration": 3000
       }
     }
   ];
