9242d0c04fde73180ee2750e5b37a40542faee00
canbax <yusufsaidcanbaz@gmail.com>
2022-02-07 18:30:24 +0300

    deploy to github-pages; rename 'graph-imager'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'graph-imager'`, () => {
+  it(`should have as title 'davraz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('graph-imager');
+    expect(app.title).toEqual('davraz');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('graph-imager app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('davraz app is running!');
   });
 });
